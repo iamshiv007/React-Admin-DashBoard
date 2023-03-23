0767b7d5f5d152c35ae18639e940d3060d5f01b5
iamshiv007 <iamshiv20032003@gmail.com>
2023-03-23 14:24:16 +0530

    feat: start projecct from scrach

--- src/index.js
+++ src/index.js
@@ -1,15 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom'
+import { App } from './App'
 
-import './index.css';
-import App from './App';
-import { ContextProvider } from './contexts/ContextProvider';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <ContextProvider>
-      <App />
-    </ContextProvider>
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
+ReactDOM.render(<App/>, document.getElementById('root'))
